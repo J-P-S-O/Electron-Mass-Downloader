40e60774fa38fb45807f79b9af3548bc9ecf5702
JPSO <jpso.guerreiro@gmail.com>
2020-12-17 11:45:45 -0300

    Del useless lines

--- src/renderer.js
+++ src/renderer.js
@@ -2,28 +2,18 @@ const fs = require('fs')
 const request = require('request')
 const path = require('path');
 const open = require('open');
-let electron = require('electron')
-let remote = electron.remote
-
+let {ipcMain} = require('electron')
 let lnamee = 0
-
-function getMime(header){
-
-		return "sorry, not available"
-}
 window.onload = function(){
     lnamee = document.getElementById("lname")
-
 console.log("included renderer")
 }
-
 let mydown = function(url){
   lnamee.value = ""
-
             // Some data that will be sent to the main process
             url = url.split(",")
             for (i in url){
-download(url[i])
+//send ipc
 
 }
 
